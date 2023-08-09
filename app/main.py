979ea131eb642b49d906628db8089f88450a6410
Zhenghong Bao <baozh166@gmail.com>
2023-08-09 04:27:13 -0400

    update main,index,requirement

--- app/main.py
+++ app/main.py
@@ -3,28 +3,37 @@ import pickle
 from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
-model = pickle.load(open("models/model_lr.pkl", "rb"))
+model = pickle.load(open("models/model_rfr.pkl", "rb"))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     res = ""
     if request.method == "POST":
-        features = [0]*7
+        features = [0]*10
 
         search_type = request.form["Search type"]
         if search_type == "blastn":
-            features[1] = 1
-            features[3] = 1
-        elif search_type == "blastp":
             features[0] = 1
+            features[6] = 1
+        elif search_type == "blastp":
+            features[1] = 1
+            features[5] = 1
+        elif search_type == "blastx":
             features[2] = 1
+            features[5] = 1
+        elif search_type == "tblastn":
+            features[3] = 1
+            features[6] = 1
+        elif search_type == "tblastx":
+            features[4] = 1
+            features[6] = 1
 
-        features[4] = request.form["file_size"]
-        features[5] = request.form["cpu"]
-        features[6] = request.form["mem"]
+        features[7] = request.form["file_size"]
+        features[8] = request.form["cpu"]
+        features[9] = request.form["mem"]
 
         df = pd.DataFrame(features).T
-        df.columns = ["search_blastp", "search_tblastn", "db_nr", "db_nt", "file_size", "cpu", "mem"]
+        df.columns = ["search_blastn", "search_blastp", "search_blastx", "search_tblastn", "search_tblastx", "db_nr", "db_nt", "file_size", "cpu", "mem"]
         prediction = model.predict(df)
         res = int(prediction[0])
         if res < 0: 
